2023ca5b662e311861f6634a6acd1f41af2534e3
zhangtao@allmedia-cn.com <zhangtao@allmedia-cn.com>
2019-04-30 10:49:41 +0800

    部署

---
+++ deploy.php
@@ -0,0 +1,27 @@
+<?php
+
+class deployment
+{
+    private $token = 'A;LS=DK+JF(*YOH(*089+F23LKJ';
+
+    public function deploy()
+    {
+        $commands = ['cd /var/www/wechat', 'git pull'];
+        $headers = getallheaders();
+        $signature = $headers['X_Hnu_Signature'];
+        $payload = file_get_contents('php://input');
+        if ($this->isFromGithub($payload, $signature)) {
+            foreach ($commands as $command) {
+                shell_exec($command);
+            }
+            http_response_code(200);
+        } else {
+            http_response_code(403);
+        }
+    }
+
+
+function isFromGithub($payload, $signature) {
+    return 'sha1=' . hash_hmac('sha1', $payload, $this->token, false) === $signature;
+}
+}
