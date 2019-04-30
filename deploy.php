0302e48c18a6f07221c4ba8691b56302595e22b4
zhangtao@allmedia-cn.com <zhangtao@allmedia-cn.com>
2019-04-30 12:30:18 +0800

    test

--- deploy.php
+++ deploy.php
@@ -7,31 +7,17 @@ class Deployment
     public function deploy()
     {
         $commands = ['cd /var/www/wechat', 'git pull'];
-//        $headers = getallheaders();
-//        $signature = $headers['X-Hub-Signature'];
-//        $payload = file_get_contents('php://input');
-//        if ($this->isFromGithub($payload, $signature)) {
-//            foreach ($commands as $command) {
-//                shell_exec($command);
-//            }
-//            http_response_code(200);
-//        } else {
-//            http_response_code(403);
-//        }
-        echo '<pre>';
-        print_r($_SERVER);echo '</pre>';
-    }
-
-    function getallheaders()
-    {
-        $headers = '';
-        foreach ($_SERVER as $name => $value)
-        {
-            if (substr($name, 0, 5) == 'HTTP_') {
-                $headers[str_replace(' ', '-', ucwords(strtolower(str_replace('_', ' ', substr($name, 5)))))] = $value;
+        file_put_contents('test.txt', $_SERVER);
+        $signature = $_SERVER['X-Hub-Signature'];
+        $payload = file_get_contents('php://input');
+        if ($this->isFromGithub($payload, $signature)) {
+            foreach ($commands as $command) {
+                shell_exec($command);
             }
+            http_response_code(200);
+        } else {
+            http_response_code(403);
         }
-        return $headers;
     }
 
     function isFromGithub($payload, $signature)
