808a93a78f01afdc6fff5d8bb75599541d70077e
zhangtao@allmedia-cn.com <zhangtao@allmedia-cn.com>
2019-04-30 17:39:52 +0800

    test

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-$commands = ['cd /var/www/wechat', 'git pull'];
+$commands = ['cd /var/www/wechat', 'sudo git pull'];
 foreach ($commands as $command) {
     $output[] = shell_exec($command);
 }
