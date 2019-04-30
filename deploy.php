93da0f1036cc61879729bdefdf10778a9509274f
zhangtao@allmedia-cn.com <zhangtao@allmedia-cn.com>
2019-04-30 12:11:24 +0800

    test

--- deploy.php
+++ deploy.php
@@ -1,33 +1,33 @@
 <?php
 
-class Deployment
-{
-    private $token = 'A;LS=DK+JF(*YOH(*089+F23LKJ';
-
-    public function deploy()
-    {
-//        $commands = ['cd /var/www/wechat', 'git pull'];
-//        $headers = getallheaders();
-//        $signature = $headers['X-Hub-Signature'];
-//        $payload = file_get_contents('php://input');
-//        if ($this->isFromGithub($payload, $signature)) {
-//            foreach ($commands as $command) {
-//                shell_exec($command);
-//            }
-//            http_response_code(200);
-//        } else {
-//            http_response_code(403);
-//        }
-        var_dump(123123);
-    }
-
-
-    function isFromGithub($payload, $signature)
-    {
-        return 'sha1=' . hash_hmac('sha1', $payload, $this->token, false) === $signature;
-    }
-}
+//class Deployment
+//{
+//    private $token = 'A;LS=DK+JF(*YOH(*089+F23LKJ';
+//
+//    public function deploy()
+//    {
+////        $commands = ['cd /var/www/wechat', 'git pull'];
+////        $headers = getallheaders();
+////        $signature = $headers['X-Hub-Signature'];
+////        $payload = file_get_contents('php://input');
+////        if ($this->isFromGithub($payload, $signature)) {
+////            foreach ($commands as $command) {
+////                shell_exec($command);
+////            }
+////            http_response_code(200);
+////        } else {
+////            http_response_code(403);
+////        }
+//        var_dump(123123);
+//    }
+//
+//
+//    function isFromGithub($payload, $signature)
+//    {
+//        return 'sha1=' . hash_hmac('sha1', $payload, $this->token, false) === $signature;
+//    }
+//}
 echo '000';
-$deploy = new Deployment();
-$deploy->deploy();
+//$deploy = new Deployment();
+//$deploy->deploy();
 echo '999';
