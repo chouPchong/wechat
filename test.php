2ba8d290669e0ea6cb3f752f8c6d00eb4fd4d5ce
zhangtao@allmedia-cn.com <zhangtao@allmedia-cn.com>
2019-04-30 17:37:09 +0800

    test

--- test.php
+++ test.php
@@ -1,3 +1,6 @@
 <?php
-$output = shell_exec('mkdir /var/www/test');
+$commands = ['cd /var/www/wechat', 'git pull'];
+foreach ($commands as $command) {
+    $output[] = shell_exec($command);
+}
 echo "<pre>$output</pre>";
